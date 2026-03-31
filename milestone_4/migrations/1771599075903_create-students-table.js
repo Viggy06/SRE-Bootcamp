bad07d3ba5eda68dda49807b1a37a908ba5ba5a3
Viggy06 <naikviggnesh@gmail.com>
2026-03-31 06:14:16 +0000

    Milestone_4

---
+++ milestone_4/migrations/1771599075903_create-students-table.js
@@ -0,0 +1,26 @@
+export const up = (pgm) => {
+  pgm.createTable("students", {
+    id: {
+      type: "bigserial",
+      primaryKey: true,
+    },
+    name: {
+      type: "varchar(100)",
+      notNull: false,
+    },
+    age: {
+      type: "integer",
+      notNull: false,
+      check: "age > 0",
+    },
+    created_at: {
+      type: "timestamp",
+      default: pgm.func("now()"),
+      notNull: true,
+    },
+  });
+};
+
+export const down = (pgm) => {
+  pgm.dropTable("students");
+};
